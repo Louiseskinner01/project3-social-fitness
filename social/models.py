62ea214e4f2928f89e211665920e1e05f0710449
Louise sKinner <louise.skinner40@gmail.com>
2025-12-21 22:28:27 +0000

    Create Django folder structure and models

---
+++ social/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    image = models.ImageField(upload_to="posts/")
+    caption = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id} by {self.user.username}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment {self.id} on Post {self.post.id} by {self.user.username}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["post", "user"], name="unique_like_per_user_per_post")
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} likes Post {self.post.id}"
