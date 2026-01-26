169cd59be1f4a481e6a18a9446a8706b77a72b97
Louise sKinner <louise.skinner40@gmail.com>
2026-01-26 19:32:48 +0000

    Add url paths and import views to link to add_comment and comment_edit views

--- social/urls.py
+++ social/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import landing, profile, PostList, create_post, delete_post, signup
+from . import views
 
 urlpatterns = [
     path("", landing, name="landing"),
@@ -8,5 +9,7 @@ urlpatterns = [
     path("posts/new/", create_post, name="create_post"),
     path("posts/<int:post_id>/delete/", delete_post, name="delete_post"),
     path("signup/", signup, name="signup"),
+    path("posts/<int:post_id>/comment/", views.add_comment, name="add_comment"),
+    path("comments/<int:comment_id>/edit/", views.comment_edit, name="comment_edit"),
 ]
 
