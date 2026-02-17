0ee9a58cefe693d8549df3572d3c60222860124b
Louise sKinner <louise.skinner40@gmail.com>
2026-02-17 13:55:51 +0000

    Restructure settings installed apps and static folder at root

--- social_fitness/settings.py
+++ social_fitness/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
-    'cloudinary',
     'django.contrib.staticfiles',
+    'cloudinary_storage',  
+    'cloudinary',
     'social',
     'django_summernote'
 ]
@@ -130,8 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
